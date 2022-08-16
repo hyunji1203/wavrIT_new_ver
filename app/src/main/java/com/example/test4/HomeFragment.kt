baf7ea5b66746979b4ed8c7c9d0474c15ec8166c
hyunji1203 <hj27720@gmail.com>
2022-08-16 15:02:09 +0900

    메인 수정

--- app/src/main/java/com/example/test4/HomeFragment.kt
+++ app/src/main/java/com/example/test4/HomeFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.viewpager.widget.ViewPager
 import com.example.test4.adapter.ViewPagerAdapter_home
+import com.example.test4.adapter.ViewPagerAdapter_home_recommend
 import com.example.test4.adapter.ViewPagerAdapter_magazine
 
 
@@ -18,19 +19,35 @@ class HomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_home, container, false)
 
-        // viewpager 어댑터
+        // viewpager 어댑터1
         val viewPager_home = view.findViewById<ViewPager>(R.id.viewPager_magazine)
         // 슬라이드를 위한 어댑터 설정
-        val pagerAdapter_middle = ViewPagerAdapter_home(childFragmentManager)
-        viewPager_home.adapter = pagerAdapter_middle
+        val pagerAdapter_home = ViewPagerAdapter_home(childFragmentManager)
+        viewPager_home.adapter = pagerAdapter_home
 
-        // 옆으로 넘어갈 때 이미지가 어떻게 보이는지 설정
-        val dpValue: Int = 0
-        val d: Float = getResources().getDisplayMetrics().density
-        val margin: Int = dpValue * d.toInt()
-        viewPager_home.setClipToPadding(false)
-        viewPager_home.setPadding(margin, 0, margin, 0);
-        viewPager_home.setPageMargin(30)
+        // viewpager 어댑터2
+        val viewPager_home_recommend = view.findViewById<ViewPager>(R.id.viewPager_home)
+        // 슬라이드를 위한 어댑터 설정
+        val pagerAdapter_recommend = ViewPagerAdapter_home_recommend(childFragmentManager)
+        viewPager_home_recommend.adapter = pagerAdapter_recommend
+
+
+        // 배너 옆으로 넘어갈 때 이미지가 어떻게 보이는지 설정
+        val dpValue1: Int = 0
+        val d1: Float = resources.displayMetrics.density
+        val margin1: Int = dpValue1 * d1.toInt()
+        viewPager_home.clipToPadding = false
+        viewPager_home.setPadding(margin1, 0, margin1, 0);
+        viewPager_home.pageMargin = 30
+
+
+        // 추천 정보 옆으로 넘어갈 때 이미지가 어떻게 보이는지 설정
+        val dpValue2: Int = 20
+        val d2: Float = resources.displayMetrics.density
+        val margin2: Int = dpValue2 * d2.toInt()
+        viewPager_home_recommend.clipToPadding = false
+        viewPager_home_recommend.setPadding(margin2, 0, margin2, 0);
+        viewPager_home_recommend.pageMargin = 30
 
         return view
     }
--- app/src/main/java/com/example/test4/adapter/ViewPagerAdapter_home_recommend.kt
+++ app/src/main/java/com/example/test4/adapter/ViewPagerAdapter_home_recommend.kt
@@ -4,28 +4,30 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
-import com.example.test4.home.Home_firstFragment
-import com.example.test4.home.Home_secondFragment
-import com.example.test4.home.Home_thirdFragment
+import com.example.test4.home.recommend.*
 
-class ViewPagerAdapter_home (fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+class ViewPagerAdapter_home_recommend (fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
 
     override fun getItem(position: Int): Fragment {
 
         return when(position) {
 
-            0       ->  Home_firstFragment()
+            0       ->  Recommend_firstFragment()
 
-            1       ->  Home_secondFragment()
+            1       ->  Recommend_secondFragment()
 
-            else       -> Home_thirdFragment()
+            2       ->  Recommend_thirdFragment()
+
+            3       ->  Recommend_fourthFragment()
+
+            else       -> Recommend_fifthFragment()
 
         }
 
     }
 
     // 생성 할 Fragment 의 개수
-    override fun getCount() = 3
+    override fun getCount() = 5
 
     override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
         super.destroyItem(container, position, `object`)
